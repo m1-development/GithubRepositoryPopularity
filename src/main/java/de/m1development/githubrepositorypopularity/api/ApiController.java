b684f7bfad04f574085b595dac2da8dcf3f44635
m1-development <dennis.public@gmx.net>
2025-09-01 15:03:06 +0200

    [GRP-3] Add new RestController endpoint "calculatePopularity" to accept request with required parameters + e2e test checking the correct parameter values passed

--- src/main/java/de/m1development/githubrepositorypopularity/api/ApiController.java
+++ src/main/java/de/m1development/githubrepositorypopularity/api/ApiController.java
@@ -1,8 +1,15 @@
 package de.m1development.githubrepositorypopularity.api;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ApiController {
@@ -10,6 +17,27 @@ public class ApiController {
     @GetMapping("/")
     public ResponseEntity<String> home() {
         String response = "Hello World";
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * @param queryString           Best match query string for repositories on github
+     * @param earliestDate          Optional filter to get repositories created on this date
+     * @param programmingLanguage   Optional filter to get repositories using the specified programming language
+     *
+     * @return                      JSON with all matching repositories and their calculated popularity score
+     */
+    @GetMapping("/calculatePopularity/{queryString}")
+    public ResponseEntity<Map<String, Object>> calculatePopularity(
+            @PathVariable String queryString,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate earliestDate,
+            @RequestParam(required = false) String programmingLanguage
+    ) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("queryString", queryString);
+        response.put("earliestDate", earliestDate);
+        response.put("programmingLanguage", programmingLanguage);
         return ResponseEntity.ok(response);
     }
 }
--- src/test/java/de/m1development/githubrepositorypopularity/api/ApiControllerE2ETest.java
+++ src/test/java/de/m1development/githubrepositorypopularity/api/ApiControllerE2ETest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiControllerE2ETest {
@@ -30,4 +30,24 @@ class ApiControllerE2ETest {
         assertEquals("Hello World", response.asString());
     }
 
+    @Test
+    public void testCalculatePopularity_withAllParams() {
+        Response response = RestAssured
+                .given()
+                .log().all()
+                .baseUri("http://localhost")
+                .port(port)
+                .pathParam("queryString", "testvalue")
+                .queryParam("earliestDate", "2025-08-30")
+                .queryParam("programmingLanguage", "Java")
+                .when()
+                .get("/calculatePopularity/{queryString}")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        assertEquals("testvalue", response.jsonPath().getString("queryString"));
+        assertEquals("2025-08-30", response.jsonPath().getString("earliestDate"));
+        assertEquals("Java", response.jsonPath().getString("programmingLanguage"));
+    }
 }
