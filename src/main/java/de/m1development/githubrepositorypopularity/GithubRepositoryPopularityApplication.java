a84081934fcdd3248012593b03a1b8dd450b0a62
m1-development <dennis.public@gmx.net>
2025-09-01 14:03:27 +0200

    [GRP-1] Initial commit with spring initializr.

---
+++ src/main/java/de/m1development/githubrepositorypopularity/GithubRepositoryPopularityApplication.java
@@ -0,0 +1,13 @@
+package de.m1development.githubrepositorypopularity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryPopularityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoryPopularityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/m1development/githubrepositorypopularity/GithubRepositoryPopularityApplicationTests.java
@@ -0,0 +1,13 @@
+package de.m1development.githubrepositorypopularity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryPopularityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
